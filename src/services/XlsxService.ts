d3d9517ee7f267ead59ff8286f7fb8d24c19f5c9
ANDRII_LIASOTA <lyasota.andriy@gmail.com>
2023-07-07 10:56:53 +0300

    small fix

--- src/services/XlsxService.ts
+++ src/services/XlsxService.ts
@@ -18,6 +18,7 @@ export class XlsxService {
         let totalWeight = 0
         let totalUSD = 0
         let totalUAH = 0
+
         products.forEach(product => {
             workbook
                 .sheet('Sheet1')
@@ -76,6 +77,7 @@ export class XlsxService {
                 totalWeight += weight
             }
         })
+
         workbook
             .sheet('Sheet1')
             .cell('G' + row)
@@ -112,113 +114,8 @@ export class XlsxService {
             .value(totalWeight)
             .style({ border: true })
             .style('fill', '808080')
+
         await workbook.toFileAsync(`${path.join(__dirname, '..', 'excel')}/order${userId ? userId : ''}.xlsx`)
-        // XlsxPopulate.fromFileAsync(path.join(__dirname, '..', 'excel') + '/template.xlsx')
-        //     .then(async (workbook: any) => {
-        //         let row = 4
-        //         let number = 1
-        //         let totalWeight = 0
-        //         let totalUSD = 0
-        //         let totalUAH = 0
-        //         products.forEach(product => {
-        //             workbook
-        //                 .sheet('Sheet1')
-        //                 .cell('A' + row)
-        //                 .value(number)
-        //                 .style({ border: true })
-        //             workbook
-        //                 .sheet('Sheet1')
-        //                 .cell('B' + row)
-        //                 .value(product.product.name)
-        //                 .style({ border: true })
-        //             workbook
-        //                 .sheet('Sheet1')
-        //                 .cell('C' + row)
-        //                 .value(product.product.sku)
-        //                 .style({ border: true })
-        //             workbook
-        //                 .sheet('Sheet1')
-        //                 .cell('D' + row)
-        //                 .value(product.product.priceUSD)
-        //                 .style({ border: true })
-        //             workbook
-        //                 .sheet('Sheet1')
-        //                 .cell('E' + row)
-        //                 .value(product.product.priceUAH)
-        //                 .style({ border: true })
-        //             workbook
-        //                 .sheet('Sheet1')
-        //                 .cell('F' + row)
-        //                 .value(product.number)
-        //                 .style({ border: true })
-        //             workbook
-        //                 .sheet('Sheet1')
-        //                 .cell('G' + row)
-        //                 .value(product.number * (product.product.priceUSD as number))
-        //                 .style({ border: true })
-        //             workbook
-        //                 .sheet('Sheet1')
-        //                 .cell('H' + row)
-        //                 .value(product.number * (product.product.priceUAH as number))
-        //                 .style({ border: true })
-        //             row += 1
-        //             number += 1
-        //             totalUSD += product.number * (product.product.priceUSD as number)
-        //             totalUAH += product.number * (product.product.priceUAH as number)
-        //             let weight = 0
-        //             if (product.product.deliveryOptions && product.product.deliveryOptions.length && product.product.deliveryOptions.some((o: any) => o.name === 'weight')) {
-        //                 const index = product.product.deliveryOptions.findIndex((o: any) => o.name === 'weight')
-        //                 weight = Number(product.product.deliveryOptions[index].value)
-        //             } else if (product.product.options && product.product.options.length && product.product.options.some((o: any) => o.name === 'weight')) {
-        //                 const index = product.product.options.findIndex((o: any) => o.name === 'weight')
-        //                 weight = Number(product.product.options[index].value)
-        //             }
-        //             if (!isNaN(weight)) {
-        //                 weight = weight * product.number
-        //                 totalWeight += weight
-        //             }
-        //         })
-        //         workbook
-        //             .sheet('Sheet1')
-        //             .cell('G' + row)
-        //             .value('Загальна сума $')
-        //             .style({ border: true })
-        //             .style({ border: true })
-        //         workbook
-        //             .sheet('Sheet1')
-        //             .cell('H' + row)
-        //             .value(totalUSD)
-        //             .style({ border: true })
-        //             .style('fill', '808080')
-
-        //         workbook
-        //             .sheet('Sheet1')
-        //             .cell('G' + (row + 1))
-        //             .value('Загальна сума ₴')
-        //             .style({ border: true })
-        //         workbook
-        //             .sheet('Sheet1')
-        //             .cell('H' + (row + 1))
-        //             .value(totalUAH)
-        //             .style({ border: true })
-        //             .style('fill', '808080')
-
-        //         workbook
-        //             .sheet('Sheet1')
-        //             .cell('G' + (row + 2))
-        //             .value('Загальна вага')
-        //             .style({ border: true })
-        //         workbook
-        //             .sheet('Sheet1')
-        //             .cell('H' + (row + 2))
-        //             .value(totalWeight)
-        //             .style({ border: true })
-        //             .style('fill', '808080')
-        //         return workbook
-        //     })
-        //     .then(async (workbook: any) => {
-        //         await workbook.toFileAsync(`${path.join(__dirname, '..', 'excel')}/order${userId ? userId : ''}.xlsx`)
-        //     })
     }
 
     static async removeXlsx(userId: string) {
